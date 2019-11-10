1c617a2d954401bca0e7aa70f7bc21dfd511f42a
SidgarPraveena <praveena.sidgar@gmail.com>
2019-11-10 13:59:21 +0530

    add added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import difflib
 import hashlib#for calculating md5hash
 #taking command line arguments in python
 def current_files():
@@ -15,7 +16,7 @@ def current_files():
     return res
   
 def extract_previous_commit_info():
-     #this will return list of file name and there hast and ther diff file n.o
+     #this will return list of file name and there hash and ther diff file n.o
     master_file=open('.mygit/master_file.txt','r')
     current_commit_num=master_file.read()
     current_commit_file_name='.mygit/commit_'+current_commit_num
@@ -26,9 +27,9 @@ def extract_previous_commit_info():
     my_map={}
     for i in range(int(num_of_file)):
         #this loop will run num_of_file times.
-        key=current_commit_file.readline()
-        value_h=current_commit_file.readline()
-        value_d=current_commit_file.readline()
+        key=current_commit_file.readline()		 #**sequence num of commit
+        value_h=current_commit_file.readline()	 #**name of that file
+        value_d=current_commit_file.readline()   #**difference index
         value=(value_h,value_d)
         my_map[key]=value
     master_file.close()
@@ -63,6 +64,38 @@ def satus():
     print('file which have to change')
     for i in not_changed_file:
         print(i)
+        
+def add(x):
+	forbiden_path=os.getcwd()+'/.mygit'
+	for root,dirs,files in os.walk(os.getcwd()):
+		temp_root=root
+		if root!=forbiden_path:
+			for i in files:
+				if(x==i):
+					#**check whether the file is different from the commited one by comparing md5 
+					rf=open('.mygit/addlist_unique.txt','r')
+					#print(temp_root+'/'+i)
+					
+					for j in enumerate(rf):		#**loop to check whether any previous adds of same file are there or not
+						if j[1] == temp_root+'/'+i+'\n':
+							print("Already exist")
+							#** write code to check whether it is different from previously added file
+							#** write code which overwrites previous temp_diff
+							return 
+					rf.close()
+					
+					af=open('.mygit/addlist_unique.txt','a')
+					af.write(temp_root+'/'+i+'\n')     #af add file path if it has not been added before
+					tempd=open('.mygit/temp_diff'+i+'.txt','w')
+					'''
+					-code to open the previous committed/added file and calculate difference
+					-storing the difference temporarily in temp_diff which will be permanently saved while committing
+					'''
+					af.close()
+					return
+	print("File not found...")		          
+					
+
 #execuion of code start from this file.
 a=sys.argv
 if a[1]=='init':
@@ -85,4 +118,11 @@ if a[1]=='init':
 elif a[1]=='status':
     print('calling the status code')
     satus()
+   
+elif a[1]=='add':
+	print('add being called')
+	print(a[2])
+	add(a[2])
+
+current_files()
      
